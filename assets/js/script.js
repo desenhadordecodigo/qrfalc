af1b2599fc00f15166b7add4523377d3f46d1067
desenhadordecodigo <desenhadordecodigo@gmail.com>
2020-06-18 12:39:17 -0300

    Modo noturno

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,43 @@ $('.owl-carousel').owlCarousel({
     dots: true,
     nav:false,
     items: 1
-})
+});
+
+//Ativar o modo noturno
+(function nightMode (){
+	const el = document.querySelectorAll('.night-mode');
+	const body = document.getElementsByTagName('BODY')[0];
+	let dataNM = body.getAttribute('data-nm');
+    let getLocalStorageNM = localStorage.getItem('data-nm');
+    let date = new Date();
+    let fullHour = date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ':' + date.getSeconds();
+
+	// Verifica se já tem algum valor armazenado em localStorage
+	getLocalStorageNM === 'true' ? body.setAttribute('data-nm', 'true') : body.setAttribute('data-nm', 'false');
+
+    // Atribui a ação ao botão
+    for(let i = 0; i < el.length; i++){
+        el[i].addEventListener('dblclick', () => {
+            dataNM = body.getAttribute('data-nm');
+
+            dataNM === 'false' ? (
+                body.setAttribute('data-nm', 'true'),
+                localStorage.setItem('data-nm', 'true')
+            ) : (
+                body.setAttribute('data-nm', 'false'),
+                localStorage.setItem('data-nm', 'false')
+            );
+        });
+    }
+    
+    if (fullHour > '21:00:00' && fullHour < '05:00:00'){
+        body.setAttribute('data-nm', 'true');
+        localStorage.setItem('data-nm', 'true')
+    } else {
+        if(localStorage.getItem('data-nm') === 'true'){
+            body.setAttribute('data-nm', 'true');
+        }else{
+            body.setAttribute('data-nm', 'false');
+        }
+    };
+}());
